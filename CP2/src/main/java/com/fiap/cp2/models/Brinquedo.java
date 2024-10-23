9a07f66de4e564bb776e4cfb100506d0451ed99f
LuAlcantara <oalcantara.lucas@gmail.com>
2024-10-23 09:56:42 -0300

    Implementação da Classe Brinquedo

--- CP2/src/main/java/com/fiap/cp2/models/Brinquedo.java
+++ CP2/src/main/java/com/fiap/cp2/models/Brinquedo.java
@@ -1,4 +1,105 @@
 package com.fiap.cp2.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "CP2_Brinquedo")
+
+
 public class Brinquedo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private int id;
+	
+	private String nome;
+	private String tipo;
+	private String classificacao;
+	private String tamanho;
+	private Double preco;
+	
+	
+	public Brinquedo() {
+		super();
+	}
+
+
+	public Brinquedo(int id, String nome, String tipo, String classificacao, String tamanho, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.classificacao = classificacao;
+		this.tamanho = tamanho;
+		this.preco = preco;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getClassificacao() {
+		return classificacao;
+	}
+
+
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+
+	public Double getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
 }
