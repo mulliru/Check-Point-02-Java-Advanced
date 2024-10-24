202a3f20294cf70d0179dce8f6a934e8f3979532
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-24 11:17:01 -0300

    Adicionando as interfaces repository e service

--- CP2/src/main/java/com/fiap/cp2/repositories/BrinquedoRepository.java
+++ CP2/src/main/java/com/fiap/cp2/repositories/BrinquedoRepository.java
@@ -1,4 +1,9 @@
 package com.fiap.cp2.repositories;
 
-public interface BrinquedoRepository {
+import com.fiap.cp2.models.Brinquedo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BrinquedoRepository extends JpaRepository<Brinquedo, Integer> {
+
 }
--- CP2/src/main/java/com/fiap/cp2/services/BrinquedoService.java
+++ CP2/src/main/java/com/fiap/cp2/services/BrinquedoService.java
@@ -1,4 +1,13 @@
 package com.fiap.cp2.services;
 
+import com.fiap.cp2.models.Brinquedo;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BrinquedoService {
+
+        Brinquedo create(Brinquedo brinquedo);
+        Optional<Brinquedo> read(Integer id);
+        List<Brinquedo> getAll();
 }
